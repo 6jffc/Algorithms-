12fdab85c198e612051df3e2ec16fc88d2786068
Nguyễn Hữu Thắng (ThangNguyen) <113509432+6jffc@users.noreply.github.com>
2025-03-20 11:17:11 +0700

    Update swap.cpp

--- swap.cpp
+++ swap.cpp
@@ -1,19 +1,17 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
+#include <utility> // for std::swap
 
-void swap(int& a, int& b){
-	int temp = a;
-	a = b;
-	b = temp;
-}
-int main(){
-	int a, b;
-	cout<<"a = ";
-	cin>>a;
-	
-	cout<<"b = ";
-	cin>>b;
-	
-	swap(a, b);
-	cout<<a<<", "<<b;
+int main() {
+    int a, b;
+    std::cout << "a = ";
+    std::cin >> a;
+
+    std::cout << "b = ";
+    std::cin >> b;
+
+    // std::swap từ thư viện <utility> để hoán đổi hai biến, thay vì tự định nghĩa hàm swap.
+    std::swap(a, b);
+    std::cout << a << ", " << b << std::endl;
+
+    return 0;
 }
